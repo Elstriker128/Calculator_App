25feaa28784ea716d2bcba2720a24d35ed1a5956
Elstriker128 <89691298+Elstriker128@users.noreply.github.com>
2023-06-21 21:46:03 +0300

    Finishing the mechanics
    Finished all the mechanics for the buttons and made sure to look up various of exceptions when calculating, such as division by zero

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,117 +20,199 @@ namespace Calculator_App
     /// </summary>
     public partial class MainWindow : Window
     {
-        private float Output = 0f;
+        /// <summary>
+        /// Declaration of the values that are going to be used when using operators
+        /// </summary>
+        private decimal FirstValue = 0;
+        private decimal SecondValue = 0;
+        private decimal FinalValue = 0;
+        private string oper = "+";
         public MainWindow()
         {
             InitializeComponent();
+            //When you launch the program you see a zero in the textbox
             CurrentOutput.Text = "0";
         }
-
+        /// <summary>
+        /// A method that initiates when you press the clear button "C" on the calculator
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
         private void CButton_Click(object sender, RoutedEventArgs e)
         {
             CurrentOutput.Clear();
             CurrentOutput.Text = "0";
             MemoryOutput.Content=string.Empty;
         }
-
+        /// <summary>
+        /// A method that initiates when you press the remainder button "%" on the calculator. It helps you calculate the remainder of a number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
         private void PRButton_Click(object sender, RoutedEventArgs e)
         {
-            MemoryOutput.Content= CurrentOutput.Text;
+            MemoryOutput.Content= CurrentOutput.Text + " %";
+            FirstValue = decimal.Parse(CurrentOutput.Text);
+            CurrentOutput.Text = "0";
+            oper = "%";
         }
-
+        /// <summary>
+        /// A method that initiates when you press the division button "/" on the calculator. It helps you calculate the division of a number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
         private void DivButton_Click(object sender, RoutedEventArgs e)
         {
+            MemoryOutput.Content = CurrentOutput.Text + " /";
+            FirstValue = decimal.Parse(CurrentOutput.Text);
+            CurrentOutput.Text = "0";
+            oper = "/";
 
         }
-
+        /// <summary>
+        /// A method that initiates when you press the deletion button "←" on the calculator. It deletes a single number from the calculator's textbox.
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
         private void RevButton_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void SevenButton_Click(object sender, RoutedEventArgs e)
-        {
-            if(CurrentOutput.Text=="0")
+            if(CurrentOutput.Text == "0")
             {
-                CurrentOutput.Text = "7";
+                return;
             }
-            else
+            if(CurrentOutput.Text != "0" && CurrentOutput.Text.Length>=1)
             {
-                CurrentOutput.Text += "7";
+                CurrentOutput.Text = CurrentOutput.Text.Remove(CurrentOutput.Text.Length - 1);
             }
-        }
-
-        private void EightButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (CurrentOutput.Text == "0")
+            if(CurrentOutput.Text.Length==0)
             {
-                CurrentOutput.Text = "8";
-            }
-            else
-            {
-                CurrentOutput.Text += "8";
+                CurrentOutput.Text = "0";
+                return;
             }
         }
-
-        private void NineButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (CurrentOutput.Text == "0")
-            {
-                CurrentOutput.Text = "9";
-            }
-            else
-            {
-                CurrentOutput.Text += "9";
-            }
-        }
-
+        /// <summary>
+        /// A method that initiates when you press the subtraction button "-" on the calculator. It helps you calculate the subtraction of a number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
         private void MinButton_Click(object sender, RoutedEventArgs e)
         {
-
+            MemoryOutput.Content = CurrentOutput.Text + " -";
+            FirstValue = decimal.Parse(CurrentOutput.Text);
+            CurrentOutput.Text = "0";
+            
+            oper = "-";
         }
-
-        private void FourButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (CurrentOutput.Text == "0")
-            {
-                CurrentOutput.Text = "4";
-            }
-            else
-            {
-                CurrentOutput.Text += "4";
-            }
-        }
-
-        private void FiveButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (CurrentOutput.Text == "0")
-            {
-                CurrentOutput.Text = "5";
-            }
-            else
-            {
-                CurrentOutput.Text += "5";
-            }
-        }
-
-        private void SixButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (CurrentOutput.Text == "0")
-            {
-                CurrentOutput.Text = "6";
-            }
-            else
-            {
-                CurrentOutput.Text += "6";
-            }
-        }
-
+        /// <summary>
+        /// A method that initiates when you press the addition button "+" on the calculator. It helps you calculate the addition of a number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
         private void PlusButton_Click(object sender, RoutedEventArgs e)
         {
-
+            MemoryOutput.Content = CurrentOutput.Text + " +";
+            FirstValue = decimal.Parse(CurrentOutput.Text);
+            CurrentOutput.Text = "0";
+            
+            oper = "+";
         }
+        /// <summary>
+        /// A method that initiates when you press the multiplication button "x" on the calculator. It helps you calculate the multiplied number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
+        private void MultButton_Click(object sender, RoutedEventArgs e)
+        {
+            MemoryOutput.Content = CurrentOutput.Text + " x";
+            FirstValue = decimal.Parse(CurrentOutput.Text);
+            CurrentOutput.Text = "0";
+            
+            oper = "*";
+        }
+        /// <summary>
+        /// A method that initiates when you press the point button "." on the calculator. It helps you add a point to a number to transform it into a decimal number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
+        private void PointButton_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentOutput.Text += ".";
+        }
+        /// <summary>
+        /// A method that initiates when you press the plus or minus button "+/-" on the calculator. It helps you transform the number into either a negative or positive version of itself
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
+        private void PlusMinButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(CurrentOutput.Text.Contains("-"))
+            {
+                CurrentOutput.Text = CurrentOutput.Text.Trim('-');
+            }
+            else
+            {
+                CurrentOutput.Text = "-" + CurrentOutput.Text;
+            }
+        }
+        /// <summary>
+        /// A method that initiates when you press the equals button "=" on the calculator. It helps you calculate the desired number using the used numbers values and operators
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            MemoryOutput.Content = string.Empty;
+            switch(oper)
+            {
+                case "+":
+                    SecondValue = decimal.Parse(CurrentOutput.Text);
+                    FinalValue = FirstValue + SecondValue;
+                    CurrentOutput.Text = FinalValue.ToString();
+                    break;
+                case "-":
+                    SecondValue = decimal.Parse(CurrentOutput.Text);
+                    FinalValue = FirstValue - SecondValue;
+                    CurrentOutput.Text = FinalValue.ToString();
+                    break;
+                case "*":
+                    SecondValue = decimal.Parse(CurrentOutput.Text);
+                    FinalValue = FirstValue * SecondValue;
+                    CurrentOutput.Text = FinalValue.ToString();
+                    break;
+                case "/":
+                    SecondValue = decimal.Parse(CurrentOutput.Text);
+                    if (SecondValue != 0)
+                    {
 
+                        FinalValue = FirstValue / SecondValue;
+                        CurrentOutput.Text = FinalValue.ToString();
+                        break;
+                    }
+                    else
+                    {
+                        MemoryOutput.Content = "Can't divide by 0";
+                        break;
+                    }
+                case "%":
+                    SecondValue = decimal.Parse(CurrentOutput.Text);
+                    if(SecondValue != 0)
+                    {
+                        FinalValue = FirstValue % SecondValue;
+                        CurrentOutput.Text = FinalValue.ToString();
+                        break;
+                    }
+                    else
+                    {
+                        MemoryOutput.Content = "Can't divide by 0";
+                        break;
+                    }
+            }
+        }
+        /// <summary>
+        /// A method that initiates when you press the number 1 button "1" on the calculator. It helps you add the number 1 to the overall number or start a new number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
         private void OneButton_Click(object sender, RoutedEventArgs e)
         {
             if (CurrentOutput.Text == "0")
@@ -142,7 +224,11 @@ namespace Calculator_App
                 CurrentOutput.Text += "1";
             }
         }
-
+        /// <summary>
+        /// A method that initiates when you press the number 2 button "2" on the calculator. It helps you add the number 2 to the overall number or start a new number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
         private void TwoButton_Click(object sender, RoutedEventArgs e)
         {
             if (CurrentOutput.Text == "0")
@@ -154,7 +240,11 @@ namespace Calculator_App
                 CurrentOutput.Text += "2";
             }
         }
-
+        /// <summary>
+        /// A method that initiates when you press the number 3 button "3" on the calculator. It helps you add the number 3 to the overall number or start a new number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
         private void ThreeButton_Click(object sender, RoutedEventArgs e)
         {
             if (CurrentOutput.Text == "0")
@@ -166,25 +256,110 @@ namespace Calculator_App
                 CurrentOutput.Text += "3";
             }
         }
-
-        private void MultButton_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// A method that initiates when you press the number 4 button "4" on the calculator. It helps you add the number 4 to the overall number or start a new number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
+        private void FourButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (CurrentOutput.Text == "0")
+            {
+                CurrentOutput.Text = "4";
+            }
+            else
+            {
+                CurrentOutput.Text += "4";
+            }
         }
-
-        private void PointButton_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// A method that initiates when you press the number 5 button "5" on the calculator. It helps you add the number 5 to the overall number or start a new number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
+        private void FiveButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (CurrentOutput.Text == "0")
+            {
+                CurrentOutput.Text = "5";
+            }
+            else
+            {
+                CurrentOutput.Text += "5";
+            }
         }
-
+        /// <summary>
+        /// A method that initiates when you press the number 6 button "6" on the calculator. It helps you add the number 6 to the overall number or start a new number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
+        private void SixButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentOutput.Text == "0")
+            {
+                CurrentOutput.Text = "6";
+            }
+            else
+            {
+                CurrentOutput.Text += "6";
+            }
+        }
+        /// <summary>
+        /// A method that initiates when you press the number 7 button "7" on the calculator. It helps you add the number 7 to the overall number or start a new number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
+        private void SevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(CurrentOutput.Text=="0")
+            {
+                CurrentOutput.Text = "7";
+            }
+            else
+            {
+                CurrentOutput.Text += "7";
+            }
+        }
+        /// <summary>
+        /// A method that initiates when you press the number 8 button "8" on the calculator. It helps you add the number 8 to the overall number or start a new number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
+        private void EightButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentOutput.Text == "0")
+            {
+                CurrentOutput.Text = "8";
+            }
+            else
+            {
+                CurrentOutput.Text += "8";
+            }
+        }
+        /// <summary>
+        /// A method that initiates when you press the number 9 button "9" on the calculator. It helps you add the number 9 to the overall number or start a new number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
+        private void NineButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentOutput.Text == "0")
+            {
+                CurrentOutput.Text = "9";
+            }
+            else
+            {
+                CurrentOutput.Text += "9";
+            }
+        }
+        /// <summary>
+        /// A method that initiates when you press the number 0 button "0" on the calculator. It helps you add the number 0 to the overall number or start a new number
+        /// </summary>
+        /// <param name="sender">an object of the object class</param>
+        /// <param name="e">an object of the RoutedEventArgs event</param>
         private void ZeroButton_Click(object sender, RoutedEventArgs e)
         {
             CurrentOutput.Text += "0";
-        }
-
-        private void EqualButton_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
